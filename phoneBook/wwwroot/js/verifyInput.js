1af831cfe31278c29422334cbf95499ca1bf5419
Lucas Zaia <lucas.zaia30@gmail.com>
2021-02-03 10:03:24 -0300

    fgfgdfggf

---
+++ phoneBook/wwwroot/js/verifyInput.js
@@ -0,0 +1,33 @@
+﻿function InputVerify() {
+    var inp = document.querySelector("#username");
+    var inpPwd = document.querySelector("#pwd")
+    var spn = document.querySelector("#err");
+    var spnpwd = document.querySelector("#errpwd");
+
+    if (inp.value.length == 0) {
+        inp.style.border = "2px solid red";
+        
+        spn.style.color = "red";
+        spn.innerHTML = "*Este campo é obrigatório";
+        inp.value = "";
+    } else {
+        inp.style.border = "";
+        spn.style.display ="none";
+    }
+    if (inpPwd.value.length == 0) {
+        inpPwd.style.border = "2px solid red";
+        spnpwd.style.color = "red";
+        spnpwd.innerHTML = "*Este campo é obrigatório";
+        inpPwd.value = "";
+    }else if (inpPwd.value.length >= 1 && inpPwd.value.length < 6) {
+        var spnpwd = document.querySelector("#errpwd");
+        spnpwd.style.color = "red";
+        spnpwd.innerHTML = "*A senha precisa ter no minimo 6 digitos";
+    } 
+    else {
+        inpPwd.style.border = "";
+        spnpwd.style.display = "none";
+    }
+    
+    
+}
